4693028d60c194d4fb06244cbf7c4ea55ec00b65
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-05-21 18:42:08 -0600

    trying seeds

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -5,3 +5,22 @@ mongoose.connect(
   process.env.MONGODB_URI ||
   "mongodb://localhost/"
 );
+
+const mountainSeed = [
+  {
+    mountain: "Aspen",
+    city: "Aspen"
+  }
+];
+
+db.Mountains
+  .remove({})
+  .then(() => db.Mountains.collection.insertMany(mountainSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  })
