674e51b08cad47f8e86c4918d823a3063b3a0821
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-06-14 15:18:52 -0600

    i love deployment

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(passport.session());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client"));
+  app.use(express.static("client/build"));
 }
 // Add routes, both API and view
 app.use(routes);
